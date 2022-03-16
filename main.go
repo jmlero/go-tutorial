4882588098cf8f24be7074ffe24008325a02a7b3
jmolero <jmlero@hotmail.com>
2022-03-16 11:17:06 +0100

    improve printf

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hola Mundo")
+	var applicationName = "Booking application"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
+
+	fmt.Printf("Initializing: %v \n", applicationName)
+	fmt.Printf("Total tickets: %v \n", conferenceTickets)
+	fmt.Printf("Remaining tickets: %v \n", remainingTickets)
+
+	// user information
+	var userName string
+	var userTickets int
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User: %v -> Tickets: %v \n", userName, userTickets)
+
 }
